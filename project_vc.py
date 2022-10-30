002727f414efec61200b5b169452d05959643c1a
marianaserrao <mserraoguilherme@gmail.com>
2022-10-30 21:06:52 +0000

    combine multiple classifiers

--- project_vc.py
+++ project_vc.py
@@ -2,14 +2,14 @@ import numpy as np
 import re
 
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import GridSearchCV, train_test_split
 from sklearn.preprocessing import FunctionTransformer
 from sklearn.naive_bayes import GaussianNB, MultinomialNB
 from sklearn.linear_model import LogisticRegression
-from sklearn.svm import LinearSVC
 from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import cross_val_score
+from sklearn.svm import LinearSVC
+from sklearn.ensemble import VotingClassifier
 
 import nltk
 from nltk.stem import PorterStemmer
@@ -21,6 +21,7 @@ nltk.download('stopwords', quiet=True)
 nltk.download('wordnet', quiet=True)
 nltk.download('omw-1.4', quiet=True)
 
+
 data_path="data/train.txt"
 labels = ["Poor", "Unsatisfactory", "Good", "VeryGood", "Excellent"]
 
@@ -29,7 +30,7 @@ def pre_process(sentence):
   # lemmatizer = WordNetLemmatizer()
   # remove line feed and tab characters
   output = re.sub(r'[\n\t]', '', sentence)
-  # output = ps.stem(inter)
+  # output = ps.stem(output)
   # output = lemmatizer.lemmatize(output)
 
   return output
@@ -39,8 +40,6 @@ with open(data_path) as f:
 
 data = [pre_process(line.split('=')[2]) for line in raw_data]
 target = [labels.index(line.split('=')[1]) for line in raw_data]
-
-X_train,X_test,y_train,y_test=train_test_split(data, target, test_size=0.2)
 
 #classifiers
 mnb = MultinomialNB(
@@ -63,43 +62,25 @@ svc = LinearSVC(
 clf_pipe = Pipeline([
   ('tfidf', TfidfVectorizer( # covert strings to numerical feature vectors (tf-idf)
     use_idf=False, # idf(t) = 1
-    ngram_range=(1,4), # extracts up to 4-grams
     lowercase=True,
-  )),    
+  )), 
   ('toarray', FunctionTransformer(
     lambda x: x.toarray(), accept_sparse=True
   )),
-  ('clf', mnb)
+  ('vc', VotingClassifier(
+    estimators=[
+      ('mnb', mnb), 
+      ('gnb', gnb), 
+      ('lr', lr), 
+      ('svc', svc)
+    ], 
+    voting='hard'
+  ))
 ])
 
 # train classifier
-clf = clf_pipe.fit(X_train, y_train)
-
-prediction = clf.predict(X_test)
-
-print("Testing Accuracy Score:",accuracy_score(y_test, prediction))
+clf = clf_pipe.fit(data, target)
 
 # get cross validation scores
 scores = cross_val_score(clf, data, target, cv=5)
-print("Cross Validation Scores:", scores, np.mean(scores))
-
-"""
-# grid of parameters for optimization search
-alpha_opt = np.arange(0.05,1.01,0.05)
-ngram_opt = [(1,n) for n in list(range(1, 7))]
-grid = {
-  "tfidf__use_idf": [True, False],
-  "tfidf__stop_words": [None, stopwords.words('english')],
-  "tfidf__ngram_range": ngram_opt,
-  "tfidf__lowercase" : [True, False],
-  "clf__fit_prior": [True, False],
-  "clf__alpha": alpha_opt,
-}
-
-# grid search for best parameters
-clf_gs = GridSearchCV(clf_pipe, grid, cv= 5)
-
-# train classifier with grid search
-clf = clf_gs.fit(data, target)
-print(clf.best_score_,)
-"""
+print(scores, np.mean(scores))
